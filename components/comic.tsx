4950edc0680ddfdf469c507d65c731482a96793b
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-22 21:45:00 +0900

    Update comic.tsx

--- components/comic.tsx
+++ components/comic.tsx
@@ -56,18 +56,18 @@ const ComicInform = styled.div`
   display:flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 4px 0.9rem;
+  padding: 1px 0.8rem;
   padding-right:4px;
 `
 const ComicImg = styled.img`
   border-radius: 6px;
-  width:calc(((100vw - 1.5rem) * 0.4) + 1px);   // 1px 여유 공간
-  height:calc((100vw - 1.5rem) * 0.4 / 7 * 10); // 7:10 비율
+  width:calc(((100vw - 1.5rem) * 0.37) + 1px);   // 1px 여유 공간
+  height:calc((100vw - 1.5rem) * 0.37 / 7 * 10); // 7:10 비율
 `
 const Title = styled.h1`
   font-size: 18px;
-  margin-top: 6px;
-  margin-bottom: 2px;
+  margin-top: 7px;
+  margin-bottom: 3px;
 `
 const Explane = styled.h2`
   font-size: 12px;

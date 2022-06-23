a07404cb4fc4b8818f404e4dfd75fcba99d76ce8
CWIN77 <cwstbp0426@gmail.com>
2022-06-23 18:24:14 +0900

    add meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,29 @@ import type { NextPage } from 'next'
 import styled from 'styled-components'
 import TopBar from '../components/topBar'
 import Comic from '../components/comic'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <Container>
+      <Head>
+        <title>CWIN App</title>
+        <meta name="description" content="cwin website" />
+        <meta name="keywords" content="react web app service" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="CWIN App" />
+        <meta property="og:site_name" content="CWIN App" />
+        <meta property="og:description" content="react web app service" />
+        <meta property="og:image" content="https://i.ibb.co/YLxBsFd/site.png" />
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:url" content="" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:title" content="CWIN App" />
+        <meta name="twitter:description" content="react web app service" />
+        <meta name="twitter:image" content="https://i.ibb.co/YLxBsFd/site.png" />
+        <link rel="canonical" href="" />
+      </Head>
       <TopBar/>
       <Comic/>
     </Container>

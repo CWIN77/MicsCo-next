95e7a6a90c0bc244db44cd1c332510e8f58dc060
CWIN <81596708+CWIN77@users.noreply.github.com>
2022-06-23 19:00:30 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,23 @@ import TopBar from '../components/topBar'
 import Comic from '../components/comic'
 import Head from 'next/head'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props:any) => {
   return (
     <Container>
       <Head>
-        <title>CWIN App</title>
+        <title>{props.title}</title>
         <meta name="description" content="cwin website" />
         <meta name="keywords" content="react web app service" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="CWIN App" />
-        <meta property="og:site_name" content="CWIN App" />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:site_name" content={props.title} />
         <meta property="og:description" content="react web app service" />
         <meta property="og:image" content="https://i.ibb.co/YLxBsFd/site.png" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="400" />
         <meta property="og:url" content="" />
         <meta property="og:locale" content="en_US" />
-        <meta name="twitter:title" content="CWIN App" />
+        <meta name="twitter:title" content={props.title} />
         <meta name="twitter:description" content="react web app service" />
         <meta name="twitter:image" content="https://i.ibb.co/YLxBsFd/site.png" />
         <link rel="canonical" href="" />
@@ -36,6 +36,14 @@ const Container = styled.div`
   min-height:100vh;
   font-size:18px;
   font-weight: 600;
-` 
+`
+
+export async function getStaticProps() {
+  return {
+    props:{
+      title:"CWIN APP12"
+    }
+  }
+}
 
 export default Home
